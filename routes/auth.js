84f3dd8a9dea6dbdb0d86301c468ae55736e3ed7
Anurag Dhote <anuragdhote392@gmail.com>
2023-01-09 21:04:16 +0530

    Finish Create User endpoint using express-validator and other checks

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,44 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import User from '../models/User.js';
-
 const router = new express.Router();
 
-// Create a user using: POST "/api/auth/" Doesn't require auth.
-router.get('/', async (req, res) => {
-    try {
-        console.log(req.body)
-        const user = new User(req.body);
+// Create a user using: POST "/api/auth/createuser". No Login required
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Enter a valid password').isLength({ min: 5 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    // typeof(errors) --> Object
+    // typeof(errors.array()) --> Object
 
-        await user.save();
-        res.status(201).send(req.body);
+    // If there are errors, return Bad Request and the errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        // Check whether the user with this email exists already
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({
+                error: 'Sorry, a user with same email exits'
+            })
+        }
+
+        // Create a new user
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+        })
+
+        res.json(user);
     } catch (e) {
-        res.status(500).send(e);
+        console.log(e.message)
+        // Logger, SQS
+        res.status(500).send('Some error occured');
     }
 });
 
